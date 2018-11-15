20149efc10358e3ec52a6f560cc9bc41e1668293
Diogenes Molinares <alex.d.m.v@hotmail.com>
2018-11-15 12:13:44 +0100

    fin model 1

--- src/codeJeu/Piece.java
+++ src/codeJeu/Piece.java
@@ -1,6 +1,7 @@
 package codeJeu;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Piece {
@@ -30,9 +31,79 @@ public class Piece {
 		return passageSecret;
 	}
 
-	public void transporterPersonnage() {
-		
+	public void transporterPersonnage(Piece[][] toutesLesPieces) {
+            for(int i = 0; i < toutesLesPieces.length ; i++){
+                
+                for(int j = 0; j < toutesLesPieces[i].length; j++)
+                {
+                    //si dans la piece il y a un passage secret 
+                    if(toutesLesPieces[i][j].isPassageSecret()){
+                        
+                        //si le personnage principal n'est pas là
+                        Iterator<Personnage> iter = personnage.iterator();
+                        boolean isPer = false;
+                        while(iter.hasNext()){
+                            Personnage per = iter.next();
+                            
+                            if(per.equals("Joueur.java")){
+                                //le personnage principal est  dans la sale
+                                isPer = true;
+                            }
+                            
+                        }
+                        
+                        //changer coordonées x et y de personnagge
+                        if(!isPer){
+                          
+                            changerCoordonneesPersonnage();
+                            
+                            
+                        }
+                        
+                        
+                    }
+                    
+                }
+                
+                
+            }
 	}
+
+    public boolean isNord() {
+        return nord;
+    }
+
+
+    public boolean isSud() {
+        return sud;
+    }
+
+
+    public boolean isEst() {
+        return est;
+    }
+
+    public boolean isOuest() {
+        return ouest;
+    }
+
+
+    public Porte[] getPorte() {
+        return porte;
+    }
+
+
+    public List<Personnage> getPersonnage() {
+        return personnage;
+    }
+
+    public void setPersonnage(List<Personnage> personnage) {
+        this.personnage = personnage;
+    }
+
+    private void changerCoordonneesPersonnage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 
